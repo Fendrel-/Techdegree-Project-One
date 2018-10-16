25d566488046476f75cf3374f05f7ae1d3d4d8e3
Fendrel- <dswift88@gmail.com>
2018-10-15 22:52:45 -0700

    Updated High Score Function
    To remove confusion, updated best_score function so that high score doesn't automatically show as 11 after the first game.

--- guessing_game.py
+++ guessing_game.py
@@ -10,21 +10,12 @@ import random
 
 def start_game():
     def game_loop():
-        def best_score(num_guesses):
-            global high_score
-            if num_guesses < high_score:
-                print('\nCongrats! You got a high score!')
-                print('Previous high score (least guesses) was {}.'.format(high_score))
-                high_score = num_guesses
-            else:
-                print('\nThe current high score (least guesses) is {}.'.format(high_score))
-
         counter = 1
         playing = True
         while playing:
             try:
-                guess = int(input('\nPick a number between 1 and 10\n>>> '))
-                if guess in range(1, 11):
+                guess = int(input('\nPick a number between 1 and 100\n>>> '))
+                if guess in range(1, 101):
                     if guess > random_number:
                         print('\nSorry that\'s incorrect.')
                         print('The number I\'m thinking of is lower than that.')
@@ -35,28 +26,41 @@ def start_game():
                         counter += 1
                     else:
                         print('\nYou got it!')
-                        print('That was the number I was thinking of!')
+                        print('That was the number I was thinking of!\n')
                         print('You guessed the number in {} tries.'.format(counter))
                         playing = False
                 else:
                     raise ValueError
             except ValueError as error:
                 print('\nSorry that\'s not a valid input. Try again.')
-        best_score(counter)
+        return counter
 
-    random_number = random.randint(1, 10)
-    game_loop()
+
+    random_number = random.randint(1, 100)
+    num_guesses = game_loop()
+    return num_guesses
+
+def best_score(num_guesses):
+    global high_score
+    if num_guesses < high_score:
+        print('\nCongrats! You got a high score!')
+        print('Previous high score (least guesses) was {}.'.format(high_score))
+        return num_guesses
+    else:
+        print('\nThe current high score (least guesses) is {}.'.format(high_score))
+        return high_score
 
 
 def new_game():
-    while True:
+    play_again = False
+    while play_again is False:
         try:
             choice = input('\nWould you like to play again? (Y)es/(N)o\n>>> ')
             if choice.upper() in ['NO', 'N']:
                 print('\nThank you for playing!')
                 exit()
             elif choice.upper() in ['YES', 'Y']:
-                start_game()
+                play_again = True
             else:
                 raise ValueError
         except ValueError as error:
@@ -64,9 +68,13 @@ def new_game():
 
 
 if __name__ == '__main__':
-    global high_score
-    high_score = 11
     print('Hello and Welcome to the Number Guessing Game!')
+    global high_score
     while True:
-        start_game()
+        num_guesses = start_game()
+        try:
+            high_score = best_score(num_guesses)
+        except NameError:
+            print('Congrats! You got a high score!')
+            high_score = num_guesses
         new_game()
